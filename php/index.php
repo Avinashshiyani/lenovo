439e39ac4146dc868f8759498cf379222135da10
unknown <avinashshiyani760@gmail.com>
2022-06-13 17:56:39 +0530

    html2 added

---
+++ php/index.php
@@ -0,0 +1,25 @@
+<?php
+    include './html.php';
+?>
+<?php
+    include './html2.php';
+?>
+<?php
+    include './html3.php';
+?>
+<?php
+    include './html4.php';
+?>
+<?php
+    include './html5.php';
+?>
+<?php
+    include './html6.php';
+?>
+<?php
+    include './html7.php';
+?>
+<?php
+    include './html8.php';
+?>
+
